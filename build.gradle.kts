ce8bfc0eb142e20a8960a52130be269fe33f03ad
hfazai <hf.fazai@gmail.com>
2022-05-28 22:45:30 +0100

    Gradle: Adding project dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2021-2022 Hichem Fazai (https://github.com/hfazai).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 
 plugins {
     // Apply the org.jetbrains.kotlin.jvm Plugin to add support for Kotlin.
@@ -12,16 +27,25 @@ repositories {
     mavenCentral()
 }
 
+val exposedVersion = project.property("exposedVersion").toString()
+val dataframeVersion = project.property("dataframeVersion").toString()
+
 dependencies {
     // Align versions of all Kotlin components
     implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
-
     // Use the Kotlin JDK 8 standard library.
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
+    // Exposed
+    implementation("org.jetbrains.exposed", "exposed-core", exposedVersion)
+    implementation("org.jetbrains.exposed", "exposed-dao", exposedVersion)
+    implementation("org.jetbrains.exposed", "exposed-jdbc", exposedVersion)
+
+    // Kotlin Dataframe
+    implementation("org.jetbrains.kotlinx", "dataframe", dataframeVersion)
+
     // Use the Kotlin test library.
     testImplementation("org.jetbrains.kotlin:kotlin-test")
-
     // Use the Kotlin JUnit integration.
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
 }
